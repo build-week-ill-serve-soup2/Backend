74b15a7d16dc2e65480a874651eb5af51ea4782d
Alfonso <alfonso.garcia2324@gmail.com>
2019-06-24 11:10:46 -0700

    Added dispatcherServlet to KitchenApplication

--- kitchen/src/main/java/local/alfonso/kitchen/KitchenApplication.java
+++ kitchen/src/main/java/local/alfonso/kitchen/KitchenApplication.java
@@ -2,6 +2,8 @@ package local.alfonso.kitchen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 public class KitchenApplication
@@ -9,7 +11,10 @@ public class KitchenApplication
 
     public static void main(String[] args)
     {
-        SpringApplication.run(KitchenApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(KitchenApplication.class, args);
+
+        DispatcherServlet dispatcherServlet = (DispatcherServlet)ctx.getBean("dispatcherServlet");
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
     }
 
 }
